5a040e0e524a5b48f7fc4261509ce8aaf8a67ca2
SnelleJaapie <jasperdekorte@gmail.com>
2021-06-08 19:59:14 +0200

    data succesvol ingeladen en afbeelding weergegeven

--- src/Ditto.js
+++ src/Ditto.js
@@ -5,13 +5,15 @@ import axios from 'axios';
 
 function Ditto() {
     const [pokemon, setPokemon] = useState(null);
-
+    console.log("state in card" ,pokemon)
     useEffect(() => {
         console.log('fetching on mount')
         async function fetchData() {
             try {
                 const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
                 console.log(response.data)
+                setPokemon(response.data);
+
             } catch (e) {
                 console.error(e)
             }
@@ -25,11 +27,10 @@ function Ditto() {
     return (
         <div>
 
-            <div>
-                <p>Hier komt Pokemon
-
-                </p>
-            </div>
+            { pokemon ? <div>
+                <h1>Pokemon</h1>
+                <img src={pokemon.sprites.front_default} alt=""/>
+            </div> : <h3>loading</h3> }
 
         </div>
     );
