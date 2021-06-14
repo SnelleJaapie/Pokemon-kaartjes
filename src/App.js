a0d71a208079730f8ac3252dbc3d38025231defc
SnelleJaapie <jasperdekorte@gmail.com>
2021-06-14 13:46:06 +0200

    PR Commit, per ongeluk op main gewerkt hele tijd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import './App.css';
 import Ditto from './Ditto'
+import axios from "axios";
 
 
 function App() {
+    const [pokenames, setPokenames] = useState(null);
+    const [limit, setLimit] = useState(20);
+
+
+
+    useEffect(() => {
+        console.log("fetchnames")
+
+        async  function fetchNames(){
+            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
+            console.log(response.data.results)
+
+            setPokenames(response.data.results);
+
+        }
+
+        fetchNames()
+
+
+
+
+    }, [])
+
+
+    console.log(pokenames)
 
   return (
+
     <div>
-        <Ditto />
+
+        {pokenames ? <div>
+            {/*<Ditto nameOfPokemon={"jigglypuff"}/>*/}
+            {/*<Ditto nameOfPokemon={"diglett"}/>*/}
+            {/*<Ditto nameOfPokemon={"bulbasaur"}/>*/}
+            {/*<Ditto nameOfPokemon={"pikachu"}/>*/}
+            {/*<Ditto nameOfPokemon={"scyther"}/>*/}
+            {pokenames.map(pokemon => {
+                return <Ditto nameOfPokemon={pokemon.name}/>
+            })}
+        </div> : <h3>Loading Pokenames for you my friend</h3> }
     </div>
   );
 }
--- src/Ditto.js
+++ src/Ditto.js
@@ -3,15 +3,17 @@ import './App.css';
 import axios from 'axios';
 
 
-function Ditto() {
-    const [pokemon, setPokemon] = useState(null);
+function Ditto(props) {
+    const [pokemon, setPokemon] = useState();
+
     console.log("state in card" ,pokemon)
+
+
     useEffect(() => {
         console.log('fetching on mount')
         async function fetchData() {
             try {
-                const response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
-                console.log(response.data)
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${props.nameOfPokemon}`)
                 setPokemon(response.data);
 
             } catch (e) {
@@ -28,12 +30,34 @@ function Ditto() {
         <div>
 
             { pokemon ? <div>
-                <h1>Pokemon</h1>
+                <h1>{pokemon?.name}</h1>
+                <div>{pokemon.abilities.map((ability) => {
+                    return <p>{ability.ability.name}</p>
+                })}</div>
                 <img src={pokemon.sprites.front_default} alt=""/>
             </div> : <h3>loading</h3> }
+
+
 
         </div>
     );
 }
 
 export default Ditto;
+
+// return (
+//         <div>
+//             {pokemon ? (
+//             <div>
+//                 <h1>{pokemon.name}</h1>
+//                 <img src={pokemon.sprites.front_default}/>
+//                 <div>{pokemon.abilities.map((ability) => {
+//                     return(<p>{ability.ability.name}</p>);
+//                 })}</div>
+//             </div>
+//                  ) : (
+//                 <h3>Loading</h3>
+//                 )}
+//         </div>
+//     );
+// }
